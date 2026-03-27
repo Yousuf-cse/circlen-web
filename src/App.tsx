bbf73ab2851a65b1e07c49260a1e6772fa8c304f
Yousuf-cse <yousuf.mallik79@gmail.com>
2026-03-27 23:44:39 +0530

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className='bg-green-600'>circeln-web</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
